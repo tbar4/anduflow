d906d4b4a2a4cda2e81460ec4898efba1f4e2db5
tbar4 <trevor.barnes91@gmail.com>
2025-12-28 20:51:31 -0800

    I really thought about it and don't want to immediately serialize data from source format into arrow. I will land first then serialize from object storage

--- etl-core/src/extract/mod.rs
+++ etl-core/src/extract/mod.rs
@@ -1,34 +1,47 @@
-use arrow::datatypes::Schema;
-use arrow::record_batch::RecordBatch;
 use async_trait::async_trait;
 use serde::de::DeserializeOwned;
-use std::collections::HashMap;
+use bytes::Bytes;
 
 pub mod error;
 pub mod rest_extractor;
 
 pub type ExtractorResult<T> = Result<T, error::ExtractorError>;
-pub type Metadata = HashMap<String, String>;
 pub struct Checkpoint(pub String);
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum ExtractFormat {
+    Json,
+    Text,
+    Bytes,
+}
 
 #[async_trait]
 pub trait Extractor {
-    // Lifecyce functions
+    // Lifecycle functions
     // Build a standard init() fn
     async fn ping(&self) -> ExtractorResult<()>;
     async fn close() -> ExtractorResult<()>;
+    
     // Data Retrieval
-    async fn extract<T: DeserializeOwned>(&self) -> ExtractorResult<T>;
-    async fn to_record_batch(&self) -> ExtractorResult<RecordBatch>;
-    // This needs to implement RecordBatchStream
-    //async fn into_stream() -> ExtractorResult<BoxStream<Self::Item>>;
-    async fn extract_batch<T>() -> ExtractorResult<Vec<T>>;
+    async fn extract<T: DeserializeOwned>(&self) -> ExtractorResult<T> {
+        self.extract_json().await
+    }
+    
+    async fn extract_json<T: DeserializeOwned>(&self) -> ExtractorResult<T>;
+    
+    async fn extract_text(&self) -> ExtractorResult<String>;
+    
+    async fn extract_bytes(&self) -> ExtractorResult<Vec<u8>>;
+    
+    async fn extract_raw(&self) -> ExtractorResult<Bytes>;
+    
     // Schema/Metadata
-    fn schema() -> Option<Schema> {
+    fn schema() -> Option<String> {
         None
     }
     fn source_name(&self) -> ExtractorResult<&str>;
-    async fn metadata(&self) -> ExtractorResult<Metadata>;
+    async fn metadata(&self) -> ExtractorResult<String>;
+    
     // Incremental/Checkpointing
     fn supports_incremental(&self) -> bool {
         false
